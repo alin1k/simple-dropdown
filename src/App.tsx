3d69507c6d42803ccf2a2af256555a5db371ca79
Alin Olteanu <olteanualin1509@gmail.com>
2024-09-07 18:18:03 +0300

    fix: clickEvent is an optional prop for the <DropdownButton/> component

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
     <Dropdown>
       <DropdownLabel>Click me!</DropdownLabel>
       <DropdownMenu>
-        <DropdownButton clickEvent={()=>console.log("first button")}>Button 1</DropdownButton>
+        <DropdownButton>Button 1</DropdownButton>
         <DropdownButton clickEvent={()=>console.log("second button")}>Button 2</DropdownButton>
         <DropdownButton clickEvent={()=>console.log("third button")}>Button 3</DropdownButton>
       </DropdownMenu>
