2160218d0192e36f1dc9a1d0dcfdcc26d6b49b85
Alin Olteanu <olteanualin1509@gmail.com>
2024-09-30 18:27:28 +0300

    docs(dropdown): Add props table

--- docs/src/app/docs/dropdown/page.tsx
+++ docs/src/app/docs/dropdown/page.tsx
@@ -1,5 +1,7 @@
 import Code from '@/components/ui/code'
 import React from 'react'
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table'
+
 
 export default function DropdownPage() {
   return (
@@ -7,6 +9,41 @@ export default function DropdownPage() {
       <div>
         <h1 className='text-5xl font-bold mb-3'>Dropdown component</h1>
         <p>This is the main component of the dropdown menu that wraps all the other components</p>
+      </div>
+      <div>
+        <h1 className='text-3xl font-bold mb-3'>Props</h1>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Prop</TableHead>
+              <TableHead>Type</TableHead>
+              <TableHead>Default</TableHead>
+              <TableHead>Values</TableHead>
+              <TableHead>Required</TableHead>
+              <TableHead>Description</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell><code className='bg-secondary px-1 rounded'>variant</code></TableCell>
+              <TableCell>string</TableCell>
+              <TableCell>light</TableCell>
+              <TableCell>light,dark</TableCell>
+              <TableCell>No</TableCell>
+              <TableCell>Dropdown color</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell><code className='bg-secondary px-1 rounded'>size</code></TableCell>
+              <TableCell>string</TableCell>
+              <TableCell>sm</TableCell>
+              <TableCell>sm,md,xl</TableCell>
+              <TableCell>No</TableCell>
+              <TableCell>Dropdown size</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+
+        <p className='mt-2'>Children components will inherit the <code className='bg-secondary px-1 rounded'>variant</code> and <code className='bg-secondary px-1 rounded'>color</code> from this component.</p>
       </div>
       <div>
         <h1 className='text-3xl font-bold mb-3'>Usage</h1>
